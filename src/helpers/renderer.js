4d411c38b382e23129f1947b9ce187b0653bf94b
Alex Karalanian <alex.karalanian@gmail.com>
2017-12-06 13:17:58 -0500

    redux scaffolding added

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom'
-import Routes from '../client/Routes'
+import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Routes from '../client/Routes';
 
 // context here is used to handle redirects and error handling.
 // static router is used specifically for SSR on server since node doesnt have a url bar / browser.
@@ -10,11 +11,16 @@ import Routes from '../client/Routes'
 // We receive the request object and use req.path to identify the route requested.
 // context is just an empty object
 
-export default req => {
+// We create the store inside of index.js file
+// We execute logic and then pass it into the provider once data load is complete
+
+export default (req, store) => {
   const content = renderToString(
-    <StaticRouter location={req.url} context={{}}>
-      <Routes />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
   );
 
   return `
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import 'babel-polyfill';
 import express from 'express';
-import renderer from './helpers/renderer'
-
+import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 const app = express();
 
 app.use(express.static('public'));
@@ -8,7 +9,11 @@ app.use(express.static('public'));
 
 // we pass request object as an argument to static router defined in our renderer file.
 app.get('*', (req, res) => {
-  res.send(renderer(req))
+  const store = createStore();
+  // some logic to initialize store
+  // then load data into store
+  // then we pass the store inside rendere after data load and initialization
+  res.send(renderer(req, store));
 })
 
 app.listen(3001, () => {
